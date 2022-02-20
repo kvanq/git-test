89a90e7ebd6e64346436ced6dd9e4f4148aaae3a
Eri <Eri@ko.com>
2022-02-20 16:26:10 +0800

    second commit

--- src/main/java/org/example/git/GitTest.java
+++ src/main/java/org/example/git/GitTest.java
@@ -7,5 +7,6 @@ package org.example.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
