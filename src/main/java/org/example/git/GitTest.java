d7f54e6cad214590b17b88a38a1b8f3ca6b5565f
Eri <Eri@ko.com>
2022-02-20 16:22:37 +0800

    first commit

---
+++ src/main/java/org/example/git/GitTest.java
@@ -0,0 +1,11 @@
+package org.example.git;
+
+/**
+ * @author an
+ * @create 2022-02-20 16:19
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
